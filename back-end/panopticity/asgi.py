0dadaedfd7eb1ad6646b08221fc70d5ac3718559
Babastienne <bpo@makina-corpus.com>
2025-02-10 21:22:48 +0100

    Rename project

--- back-end/panopticity/asgi.py
+++ back-end/panopticity/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for justsmile project.
+ASGI config for panopticity project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'justsmile.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'panopticity.settings')
 
 application = get_asgi_application()
--- back-end/panopticity/wsgi.py
+++ back-end/panopticity/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for justsmile project.
+WSGI config for panopticity project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'justsmile.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'panopticity.settings')
 
 application = get_wsgi_application()
