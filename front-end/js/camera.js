bd491a4d9fb7473d1db3b27c69e5ded3f723940c
Babastienne <bpo@makina-corpus.com>
2025-02-18 21:37:35 +0100

    [ADD] Front-end list of all camera tags. Translate details of cameras

--- front-end/js/camera.js
+++ front-end/js/camera.js
@@ -1,3 +1,208 @@
+choicesCameraType = {
+  title: TEXTS.cameraTypeQuestion,
+  name: TEXTS.cameraTypeName,
+  display: true,
+  tagName: "camera:type",
+  options: {
+    dome: {
+      name: TEXTS.cameraTypeDome,
+      picture: "images/contrib/type-dome.svg",
+    },
+    fixed: {
+      name: TEXTS.cameraTypeFixed,
+      picture: "images/contrib/type-fixed.svg",
+    },
+    panning: {
+      name: TEXTS.cameraTypePanning,
+      picture: "images/contrib/type-fixed.svg",
+    },
+  },
+};
+
+choicesSurveillanceType = {
+  title: TEXTS.cameraSurveillanceQuestion,
+  name: TEXTS.cameraSurveillanceName,
+  display: true,
+  tagName: "surveillance",
+  options: {
+    public: {
+      name: TEXTS.cameraSurveillancePublic,
+      picture: "images/contrib/type-public.svg",
+    },
+    outdoor: {
+      name: TEXTS.cameraSurveillanceOutdoor,
+      picture: "images/contrib/type-outdoor.svg",
+    },
+    indoor: {
+      name: TEXTS.cameraSurveillanceIndoor,
+      picture: "images/contrib/type-indoor.svg",
+    },
+  },
+};
+
+choicesCameraMount = {
+  title: TEXTS.cameraMountQuestion,
+  name: TEXTS.cameraMountName,
+  display: true,
+  tagName: "camera:mount",
+  options: {
+    wall: {
+      name: TEXTS.cameraMountWall,
+      picture: "images/contrib/mount-wall.jpg",
+    },
+    pole: {
+      name: TEXTS.cameraMountPole,
+      picture: "images/contrib/mount-pole.jpg",
+    },
+    ceiling: {
+      name: TEXTS.cameraMountCeiling,
+      picture: "images/contrib/mount-ceiling.jpg",
+    },
+    street_lamp: {
+      name: TEXTS.cameraMountStreetLamp,
+      picture: "images/contrib/mount-streetlamp.jpg",
+    },
+    traffic_signals: {
+      name: TEXTS.cameraMountTrafficSignal,
+      picture: "images/contrib/mount-trafficlights.jpg",
+    },
+    doorbell: {
+      name: TEXTS.cameraMountDoorbell,
+      picture: "images/contrib/type-fixed.svg",
+    },
+    atm: {
+      name: TEXTS.cameraMountAtm,
+      picture: "images/contrib/type-fixed.svg",
+    },
+  },
+};
+
+choicesCameraZone = {
+  title: TEXTS.cameraZoneQuestion,
+  name: TEXTS.cameraZoneName,
+  display: true,
+  tagName: "surveillance:zone",
+  options: {
+    traffic: {
+      name: TEXTS.cameraZoneTraffic,
+      picture: "images/contrib/zone-traffic.svg",
+    },
+    town: {
+      name: TEXTS.cameraZoneTown,
+      picture: "images/contrib/zone-city.jpg",
+    },
+    entrance: {
+      name: TEXTS.cameraZoneEntrance,
+      picture: "images/contrib/zone-entrance.png",
+    },
+    shop: {
+      name: TEXTS.cameraZoneShop,
+      picture: "images/contrib/zone-store.jpg",
+    },
+    bank: {
+      name: TEXTS.cameraZoneBank,
+      picture: "images/contrib/zone-bank2.svg",
+    },
+    building: {
+      name: TEXTS.cameraZoneBuilding,
+      picture: "images/contrib/zone-building.svg",
+    },
+    parking: {
+      name: TEXTS.cameraZoneParking,
+      picture: "images/contrib/zone-parking.svg",
+    },
+    public_transport_platform: {
+      name: TEXTS.cameraZonePublicTransportPlatform,
+      picture: "images/contrib/zone-transport.jpg",
+    },
+  },
+};
+
+choicesCameraHeight = {
+  title: TEXTS.cameraHeightQuestion,
+  name: TEXTS.cameraHeightName,
+  display: true,
+  tagName: "height",
+  defaultValue: 4,
+  minValue: 1,
+  maxValue: 16,
+  step: 0.5,
+};
+
+choicesCameraDirection = {
+  title: TEXTS.cameraDirectionQuestion,
+  name: TEXTS.cameraDirectionName,
+  display: true,
+  tagName: "camera:direction",
+  additionalTransform: 0,
+  defaultValue: 0,
+  minValue: 0,
+  maxValue: 355,
+  step: 5,
+};
+
+choicesCameraAngle = {
+  title: TEXTS.cameraAngleQuestion,
+  name: TEXTS.cameraAngleName,
+  display: true,
+  tagName: "camera:angle",
+  additionalTransform: 90,
+  defaultValue: 15,
+  minValue: 0,
+  maxValue: 90,
+  step: 5,
+};
+
+choicesCameraLocation = {
+  title: TEXTS.cameraLocationQuestion,
+  name: TEXTS.cameraLocationName,
+  display: false,
+  tagName: "camera:position",
+};
+
+last_date = {
+  name: TEXTS.cameracheckDateName,
+  display: true,
+};
+
+tagsListCamera = {
+  man_made: { display: false },
+  "surveillance:type": { display: false },
+  check_date: last_date,
+  "survey:date": last_date,
+  source: {
+    name: TEXTS.source,
+    display: true,
+  },
+  operator: {
+    name: TEXTS.operator,
+    display: true,
+  },
+  "contact:webcam": {
+    name: TEXTS.cameraWebcamName,
+    display: true,
+  },
+  "addr:street": {
+    name: TEXTS.address,
+    display: true,
+  },
+  name: {
+    name: TEXTS.name,
+    display: true,
+  },
+  "camera:type": choicesCameraType,
+  surveillance: choicesSurveillanceType,
+  "camera:mount": choicesCameraMount,
+  "surveillance:zone": choicesCameraZone,
+  height: choicesCameraHeight,
+  ele: choicesCameraHeight,
+  "camera:angle": choicesCameraAngle,
+  "camera:position": choicesCameraLocation,
+  "camera:direction": choicesCameraDirection,
+  "surveillance:direction": choicesCameraDirection,
+  direction: choicesCameraDirection,
+};
+
 async function getCameraDetails(idCamera) {
   const url = `${BASE_URL_API}/cameras/${idCamera}.json`;
   try {
@@ -15,6 +220,34 @@ async function displayCameraDetails(e) {
   addCameraDetailsData(e.target, cameraDetails);
 }
 
+function _transformTagContentInHtml(content) {
+  let descr = String(content);
+  if (descr.substring(0, 4) == "http") {
+    var suffix = descr.slice(-3).toLowerCase();
+    if (suffix == "jpg" || suffix == "gif" || suffix == "png") {
+      return `<a href="${descr}"><img alt="Link" src="${descr}" width="200"/></a>`;
+    } else {
+      return `<a href="${descr}">${TEXTS.link}</a>`;
+    }
+  } else {
+    return content;
+  }
+}
+
+function _createTableEntry(key, value) {
+  let preparedValue = _transformTagContentInHtml(value);
+  return `
+        <tr>
+          <td>
+            ${key}
+          </td>
+          <td>
+            ${preparedValue}
+          </td>
+        <td>
+      `;
+}
+
 // Add camera popup to camera marker.
 function addCameraDetailsData(plotMarker, plot) {
   let { lat, lng } = plotMarker.getLatLng();
@@ -23,7 +256,7 @@ function addCameraDetailsData(plotMarker, plot) {
   <table class="pico modal-table">
       <thead>
         <tr>
-          <th>Camera id</th>
+          <th>${TEXTS.identifier}</th>
           <th>
               <a target="blank" href="https://www.openstreetmap.org/node/${plot.id}">${plot.id}</a>
           </th>
@@ -31,34 +264,38 @@ function addCameraDetailsData(plotMarker, plot) {
       </thead>
       <tbody>
         <tr>
-          <td>latitude</td><td>${lat}</td>
+          <td>Latitude</td><td>${lat}</td>
         </tr>
         <tr>
-          <td>longitude</td><td>${lng}</td>
+          <td>Longitude</td><td>${lng}</td>
         </tr>`;
-  for (x in plot) {
-    if (!["", "null"].includes(String(plot[x])) && !["id"].includes(x)) {
-      popupDataTable = popupDataTable + "<tr><td>" + x + "</td><td>";
-      var descr = String(plot[x]);
-      if (descr.substring(0, 4) == "http") {
-        var suffix = descr.slice(-3).toLowerCase();
-        if (suffix == "jpg" || suffix == "gif" || suffix == "png") {
-          popupDataTable = `${popupDataTable}<a href="${descr}"><img alt="Link" src="${descr}" width="200"/></a>`;
+  console.log(plot);
+  for (x in plot.tags) {
+    if (Object.keys(tagsListCamera).includes(x)) {
+      if (tagsListCamera[x].display) {
+        if (tagsListCamera[x]?.options?.[plot.tags[x]]) {
+          popupDataTable =
+            popupDataTable +
+            _createTableEntry(
+              tagsListCamera[x].name,
+              tagsListCamera[x].options[plot.tags[x]].name
+            );
         } else {
-          popupDataTable = `${popupDataTable}<a href="${descr}">Link</a>`;
+          popupDataTable =
+            popupDataTable +
+            _createTableEntry(tagsListCamera[x].name, plot.tags[x]);
         }
-      } else {
-        popupDataTable = popupDataTable + plot[x];
       }
-      popupDataTable = popupDataTable + "</td></tr>";
-      listAttributes.push(x);
+    } else {
+      popupDataTable = popupDataTable + _createTableEntry(x, plot.tags[x]);
     }
+    listAttributes.push(x);
   }
   popupDataTable = popupDataTable + "</tbody></table>";
   if (OSM.isLoggedIn()) {
     if (
-      listAttributes.length < 6 ||
-      (["fixed", "panning"].includes(plot["camera_type"]) && listAttributes < 8)
+      listAttributes.length < 7 ||
+      (["fixed", "panning"].includes(plot["camera_type"]) && listAttributes < 9)
     ) {
       popupDataTable =
         popupDataTable +
