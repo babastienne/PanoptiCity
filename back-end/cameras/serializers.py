a006d3e7c2845c109f19acca44de3e3ed6e2b0b4
babastienne <16348048+babastienne@users.noreply.github.com>
2025-12-21 01:35:42 +0100

    [🛀 Style/Clean up] Do not send focus or color for camera list anymore
    Not needed since the focus are now send via vector tiles directly

--- back-end/cameras/serializers.py
+++ back-end/cameras/serializers.py
@@ -5,7 +5,6 @@ from rest_framework import serializers
 class CameraListSerializer(serializers.HyperlinkedModelSerializer):
     lat = serializers.SerializerMethodField()
     lon = serializers.SerializerMethodField()
-    focus = serializers.SerializerMethodField()
 
     def get_lat(self, obj):
         return round(obj.location.y, 6)
@@ -13,24 +12,9 @@ class CameraListSerializer(serializers.HyperlinkedModelSerializer):
     def get_lon(self, obj):
         return round(obj.location.x, 6)
 
-    def get_focus(self, obj):
-        focus = obj.focus
-        return (
-            [[round(point[1], 6), round(point[0], 6)] for point in focus[0]]
-            if focus
-            else None
-        )
-
-    def __init__(self, *args, **kwargs):
-        # Don't return focus and color when not explicitly asked with query_param 'focus'
-        if "focus" not in kwargs["context"]["request"].query_params:
-            del self.fields["focus"]
-            del self.fields["color"]
-        super().__init__(*args, **kwargs)
-
     class Meta:
         model = Camera
-        fields = ["id", "lon", "lat", "color", "marker", "focus"]
+        fields = ["id", "lon", "lat", "marker"]
 
 
 class CameraDetailSerializer(serializers.HyperlinkedModelSerializer):
